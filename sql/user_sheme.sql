37a9598ad65520549eb05e1f87b7d28d2d59a6d0
Ifutryucatch <44875805+Ifutryucatch@users.noreply.github.com>
2019-04-24 21:12:29 +0200

    Update user_sheme.sql

--- sql/user_sheme.sql
+++ sql/user_sheme.sql
@@ -3,12 +3,12 @@ CREATE SCHEMA user_sheme DEFAULT CHARACTER SET utf8;
 USE user_sheme;
 
 CREATE TABLE user_info(
-	user_id INT auto_increment unique,
-    user_username varchar(500) unique not null,
-    user_password varchar(400) not null,
-    
-    primary key(user_id)
+	user_id INT AUTO_INCREMENT UNIQUE,
+    user_username VARCHAR(100) UNIQUE NOT NULL,
+    user_password VARCHAR(100) NOT NULL,
+    user_role VARCHAR(100) NOT NULL,
+    PRIMARY KEY(user_id)
 );
 
-insert into user_info(user_id, user_username, user_password) values(1, 'Ivkonymax', 'Pringles98');
-insert into user_info(user_username, user_password) values('mamamia', '123456');
+INSERT INTO user_info(user_id, user_username, user_password, user_role) VALUES (1, 'Ivkonymax', 'Pringles98', 'admin');
+INSERT INTO user_info(user_username, user_password, user_role) VALUES ('mamamia', '12345678', 'user');
