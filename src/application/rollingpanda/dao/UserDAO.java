9a30ca870a8236aec70f79e38e8786fe3d2d826a
Ifutryucatch <44875805+Ifutryucatch@users.noreply.github.com>
2019-04-24 21:12:17 +0200

    Update UserDAO.java

--- src/application/rollingpanda/dao/UserDAO.java
+++ src/application/rollingpanda/dao/UserDAO.java
@@ -4,10 +4,66 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import application.rollingpanda.models.User;
 
 public class UserDAO {
+	
+	public static boolean delete(Connection conn, boolean id) {
+		Statement stmt = null;
+		try {
+			
+			String query = "DELETE FROM user_info WHERE user_id = " + id;
+			
+			stmt = conn.createStatement();
+			return stmt.executeUpdate(query) == 1;
+			
+		}catch (SQLException ex) {
+			System.out.println("Greska u SQL upitu!");
+			ex.printStackTrace();
+		} finally {
+			try {stmt.close();} catch (SQLException ex1) {ex1.printStackTrace();}
+		}
+		
+		return false;
+	}
+	
+	public static List<User> getAll(Connection conn){
+		List<User> retVal = new ArrayList<>();
+		
+		Statement stmt = null;
+		ResultSet rset = null;
+		
+		try {
+			
+			String query = "SELECT user_username, user_password, user_role FROM user_info";
+			
+			stmt = conn.createStatement();
+			rset = stmt.executeQuery(query);
+			
+			while(rset.next()) {
+				int index = 1;
+				String username = rset.getString(index++);
+				String password = rset.getString(index++);
+				String role = rset.getString(index++);
+				
+				User u = new User(username, password, role);
+				retVal.add(u);
+			}
+			
+		}catch (SQLException ex) {
+			System.out.println("Greska u SQL upitu!");
+			ex.printStackTrace();
+		} finally {
+			try {stmt.close();} catch (SQLException ex1) {ex1.printStackTrace();}
+			try {rset.close();} catch (SQLException ex1) {ex1.printStackTrace();}
+		}
+		
+		return retVal;
+	}
 	
 	public static User getUserUsername(Connection conn, String username) {
 		User retVal = null;
@@ -16,7 +72,7 @@ public class UserDAO {
 		ResultSet rset = null;
 		
 		try {
-			String query = "SELECT user_id, user_password FROM user_info WHERE user_username = ?";
+			String query = "SELECT user_id, user_password, user_role FROM user_info WHERE user_username = ?";
 			
 			pstmt = conn.prepareStatement(query);
 			int index = 1;
@@ -24,10 +80,11 @@ public class UserDAO {
 			rset = pstmt.executeQuery();
 			
 			if(rset.next()) {
-				index = 1;
+				index = 2;
 				String password = rset.getString(index++);
+				String role = rset.getString(index++);
 				
-				retVal = new User(password, username);				
+				retVal = new User(username, password, role);				
 			}
 			
 		}catch (SQLException ex) {
@@ -47,7 +104,7 @@ public class UserDAO {
 		ResultSet rset = null;
 		
 		try {
-			String query = "SELECT user_id, user_username FROM user_info WHERE user_password = ?";
+			String query = "SELECT user_id, user_username, user_role FROM user_info WHERE user_password = ?";
 			
 			pstmt = conn.prepareStatement(query);
 			int index = 1;
@@ -55,10 +112,11 @@ public class UserDAO {
 			rset = pstmt.executeQuery();
 			
 			if(rset.next()) {
-				index = 1;
+				index = 2;
 				String username = rset.getString(index++);
+				String role = rset.getString(index++);
 				
-				retVal = new User(password, username);				
+				retVal = new User(username, password, role);				
 			}
 			
 		}catch (SQLException ex) {
@@ -75,7 +133,7 @@ public class UserDAO {
 		PreparedStatement pstmt = null;
 		
 		try {
-			String query = "INSERT INTO user_info(user_username, user_password) VALUES (?, ?)";
+			String query = "INSERT INTO user_info(user_username, user_password, user_role) VALUES (?, ?, ?)";
 			
 			pstmt = conn.prepareStatement(query);
 			
@@ -83,6 +141,7 @@ public class UserDAO {
 			
 			pstmt.setString(index++, user.getUsername());
 			pstmt.setString(index++, user.getPassword());
+			pstmt.setString(index++, user.getRole());
 			
 			return pstmt.executeUpdate() == 1;
 			
